546763019d83ef2fa999ebfa099f11ce7f58d1ca
Nelson <neor9712@gmail.com>
2020-03-04 20:57:57 -0600

    screen component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import  Header  from './components/Header'
+import  StartGameScreen  from './components/StartGameScreen'
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number"/>
+      <StartGameScreen />
     </View>
   );
 }
@@ -13,5 +15,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1
-  },
+  }
 });
